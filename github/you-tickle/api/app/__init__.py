13b05d73ea76c46ec5b5da3981dca0540e7acc30
cider_sweet <victor.zakharenko@yahoo.com>
2021-01-25 01:26:11 +0200

    your-tickle-db connect and redis debug

--- github/you-tickle/api/app/__init__.py
+++ github/you-tickle/api/app/__init__.py
@@ -19,7 +19,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(config_class=Config):
-    app = Flask(__name__, static_folder='../build', static_url_path = '/', template_folder='../build')
+    app = Flask(__name__, static_folder='../../build', static_url_path = '/', template_folder='../../build')
     app.config.from_object(config_class)
 
     db.init_app(app)
